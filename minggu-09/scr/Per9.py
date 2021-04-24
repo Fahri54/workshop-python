018a89e682b2cc78699f8675c1d5871143b1e678
Fahri54 <44997942+Fahri54@users.noreply.github.com>
2021-04-25 06:10:25 +0700

    Add files via upload

---
+++ minggu-09/scr/Per9.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+python3 -m venv tutorial-env
+
+
+# In[ ]:
+
+
+tutorial-env\Scripts\activate.bat
+
+
+# In[ ]:
+
+
+python
+import sys
+sys.path
+
+
+# In[ ]:
+
+
+pip install pillow
+
+
+# In[ ]:
+
+
+
+
