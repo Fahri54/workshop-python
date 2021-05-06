449a95d7dca1cf1681d9a013fbf37e163ec51b77
Fahri54 <44997942+Fahri54@users.noreply.github.com>
2021-05-06 22:19:50 +0700

    Add files via upload

---
+++ minggu-11/scr/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# save this as app.py
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
